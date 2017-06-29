f3b688466d7e8ecdfbb93050c9783d5be5743674
Przemek <przemek.b66@gmail.com>
2017-06-29 20:09:04 +0200

    move $http inside directive

--- app/app.js
+++ app/app.js
@@ -1,74 +1,87 @@
 angular.module('newWeather', [])
 
-.controller('mainController', ['getWeatherService', function(getWeatherService) {
-    console.log('main controller running', api_key);
+.controller('weatherDashboardController', [function() {
     var vm = this;
-    getWeatherService.fiveDaysBroadcast();
-    vm.value = 22;
+    vm.cities = ['Barcelona'];
+    vm.weather = {};
 }])
 
-.factory('greeter', function() {
-    return {
-        getGreeting: function(name) {
-            return "Hello " + name;
-        }
-    };
-})
 
 .directive('weatherWidget', function() {
     return {
-        templateUrl: 'weather-widget.html'
-    }
+        templateUrl: 'weather-widget.html',
+        scope: {
+            city: '@'
+        },
+        controller: ['$scope', '$http', function($scope, $http) {
+            // getWeatherService.fiveDaysBroadcast()
+
+            var url = `http://api.openweathermap.org/data/2.5/forecast/daily?q=Barcelona&cnt=5&units=metric&appid=${api_key}`;
+            var weather;
+
+            $http({
+                method: 'GET',
+                url: url
+            }).then(function successCallback(response) { // on success
+                    weather = {
+                        today: response.data.list.shift(),
+                        forecast: response.data.list
+                    }
+                    console.info(weather)
+                    $scope.weather = weather;
+                },
+                function errorCallback(response) { // on error
+                    console.log(response);
+                });
+        }],
+}
 })
 
 // get weather
 //
-.service('getWeatherService', function($http) {
-    this.fiveDaysBroadcast = function() {
-        // api.openweathermap.org/data/2.5/forecast/daily?q=Barcelona,ES&cnt=5&units=metric&appid=
-        // api.openweathermap.org/data/2.5/forecast?q=London,us&units=metric&appid=" + api_key 3h + 5 days
-        // var url = "http://api.openweathermap.org/data/2.5/forecast/daily?id=" + cities[id] + "&units=metric&cnt=7&appid=" + key;
-        var url = `http://api.openweathermap.org/data/2.5/forecast/daily?q=Barcelona&cnt=5&units=metric&appid=${api_key}`;
-        var today;
-        var forecast
+// .service('getWeatherService', function($http) {
+//     this.fiveDaysBroadcast = function() {
+//         var url = `http://api.openweathermap.org/data/2.5/forecast/daily?q=Barcelona&cnt=5&units=metric&appid=${api_key}`;
+//         var weather;
 
-        $http({
-            method: 'GET',
-            url: url
-        }).then(function successCallback(response) { // on success
+//         $http({
+//             method: 'GET',
+//             url: url
+//         }).then(function successCallback(response) { // on success
+//             return response;
+//                 // weather = {
+//                 //     today: response.data.list.shift(),
+//                 //     forecast: response.data.list
+//                 // }
+//                 // return weather;
+//             },
+//             function errorCallback(response) { // on error
+//                 console.log(response);
+//             });
 
-                today = response.data.list.shift();
-                forecast = response.data.list;
+//         // response.list.forEach(function(el) {
+//         //     data.push(round(el.temp.day));
+//         // });
 
-                console.log(today)
+//         // response.list.forEach(function(el) {
+//         //     days.push(day(el.dt));
+//         // });
 
-            },
-            function errorCallback(response) { // on error
-                console.log(response);
-            });
+//         // weather.humidity = response.list[0].humidity + "%";
+//         // weather.clouds = response.list[0].clouds + "%";
+//         // weather.pressure = response.list[0].pressure + " hPa";
+//         // weather.snow = response.list[0].snow || "brak opadów";
+//         // weather.rain = response.list[0].rain || "brak opadów";
+//         // weather.speed = response.list[0].speed + " m/s";
 
-        // response.list.forEach(function(el) {
-        //     data.push(round(el.temp.day));
-        // });
+//         // weather.day = round(response.list[0].temp.day) + "°C";
+//         // weather.max = round(response.list[0].temp.max) + "°C";
+//         // weather.min = round(response.list[0].temp.min) + "°C";
+//         // weather.night = round(response.list[0].temp.night) + "°C";
 
-        // response.list.forEach(function(el) {
-        //     days.push(day(el.dt));
-        // });
-
-        // weather.humidity = response.list[0].humidity + "%";
-        // weather.clouds = response.list[0].clouds + "%";
-        // weather.pressure = response.list[0].pressure + " hPa";
-        // weather.snow = response.list[0].snow || "brak opadów";
-        // weather.rain = response.list[0].rain || "brak opadów";
-        // weather.speed = response.list[0].speed + " m/s";
-
-        // weather.day = round(response.list[0].temp.day) + "°C";
-        // weather.max = round(response.list[0].temp.max) + "°C";
-        // weather.min = round(response.list[0].temp.min) + "°C";
-        // weather.night = round(response.list[0].temp.night) + "°C";
-
-    }
-})
+//     }
+// }
+// )
 
 
 //  run server 
