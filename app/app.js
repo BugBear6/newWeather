5cd981aa85daf9f823ee3537a635c3ff621b0567
Przemek <przemek.b66@gmail.com>
2017-07-03 15:25:26 +0200

    add weatherIcon filter (start)

--- app/app.js
+++ app/app.js
@@ -1,85 +1,114 @@
-angular.module('newWeather', [])
+angular.module('newWeather', ['ngSanitize'])
 
-    .controller('weatherDashboardController', [function() {
-        var vm = this;
+.controller('weatherDashboardController', [function() {
+	var vm = this;
 
-        vm.cities = [{
-            cityName: 'Madrid'
-        }, {
-            cityName: 'Barcelona'
-        }];
+	vm.cities = [{
+		cityName: 'Madrid'
+	}, {
+		cityName: 'Barcelona'
+	}];
 
-        vm.newCityFormData = {};
+	vm.newCityFormData = {};
 
-        vm.addCity = function() {
-        	console.log(!vm.newCityFormData.newCityName);
-        	if (!vm.newCityFormData.newCityName) {
-        		return
-        	}
+	vm.addCity = function() {
+		if (!vm.newCityFormData.newCityName) {
+			return
+		}
 
-        	var newCity = {
-        		cityName: vm.newCityFormData.newCityName,
-        		country: vm.newCityFormData.newCityCountry
-        	}
-        	vm.cities.push(newCity);
+		var newCity = {
+			cityName: vm.newCityFormData.newCityName,
+			country: vm.newCityFormData.newCityCountry
+		}
+		vm.cities.push(newCity);
 
-        	vm.newCityForm.$setPristine();
-        	vm.newCityForm.$setUntouched();
-        	vm.newCityFormData = {};
-        };
+		vm.newCityForm.$setPristine();
+		vm.newCityForm.$setUntouched();
+		vm.newCityFormData = {};
+	};
 
-        vm.closeWidget = function (index) {
-        	console.log(index);
-        	vm.cities.splice(index, 1);
-        }
+	vm.closeWidget = function(index) {
+		vm.cities.splice(index, 1);
+	}
 
-    }])
+}])
 
-    .directive('weatherWidget', function(getWeatherService, $rootScope) {
-        return {
-            templateUrl: 'weather-widget.html',
-            scope: {
-                city: '=',
-                closeWidget: '&'
-            },
-            link: function(scope, elem, attrs) {
+.filter('weatherIcon', function($rootScope, $timeout) {
+	return function(item) {
+		// var template = `<object type="image/svg+xml" data="img/weather-icons-set/CLOUDS/CLOUDS/001lighticons-02.svg" width="100" height="100">test</object>`; 
+		// var template = `<div ng-include="img/weather-icons-set/CLOUDS/CLOUDS/001lighticons-02.svg"></div>`; 
+		var template = `<img src="img/weather-icons-set/CLOUDS/CLOUDS/001lighticons-02.svg">`; 
+		// $rootScope.$on('weatherUpdate', function() {
+		console.log('after broadcast', item);
+		// switch (item) {
+		// 	case 800: // clear sky
+		// 		template = `<ng-include="img/weather-icons-set/CLOUDS/CLOUDS/001lighticons-02.svg"></ng-include>` ;
+		// 		break;
+		// 	case 801: // few clouds
+		// 		template = `img/weather-icons-set/CLOUDS/CLOUDS/001lighticons-08.svg`;
+		// 		break;
+		// 	case 802: // scatered clouds
+		// 		template = `img/weather-icons-set/CLOUDS/CLOUDS/001lighticons-14.svg`;
+		// 		break;
+		// }
 
-                getWeatherService.forSixDays(scope.city).then(
-                    function onSuccess(response) {
+		return template
+		// $timeout(function(){
+		// 	scope.$apply();
+		// }, 100);				
+		// });
+		// scope.template = `img/weather-icons-set/CLOUDS/CLOUDS/001lighticons-01.svg`;
+	}
+})
 
-                        console.log('from factory to controller', response)
+.directive('weatherWidget', function(getWeatherService, $rootScope) {
+	return {
+		templateUrl: 'weather-widget.html',
+		scope: {
+			city: '=',
+			closeWidget: '&'
+		},
+		link: function(scope, elem, attrs) {
 
-                        scope.weather = {
-                            today: response.data.list.shift(),
-                            forecast: response.data.list
-                        }
-                        scope.location = {
-                            city: response.data.city.name,
-                            country: response.data.city.country
-                        }
-                    },
-                    function onError(response) {
-                        console.log(response)
-                    });
+			getWeatherService.forSixDays(scope.city).then(
+				function onSuccess(response) {
 
-            }
-        }
-    })
+					console.log('from factory to controller', response)
 
-    .factory('getWeatherService', function($http) {
-        return {
-            forSixDays: function(city) {
-            	var cityName = city.cityName
-            	var country = (typeof city.country !== 'undefined' && city.country) ? ',' + city.country : '';
-                var url = `http://api.openweathermap.org/data/2.5/forecast/daily?q=${cityName}${country}&cnt=6&units=metric&appid=${api_key}`;
+					scope.weather = {
+						today: response.data.list.shift(),
+						forecast: response.data.list
+					}
+					scope.location = {
+						city: response.data.city.name,
+						country: response.data.city.country
+					}
 
-                return $http({
-                    method: 'GET',
-                    url: url
-                });
-            }
-        }
-    })
+					scope.initOver = true;
+					// $rootScope.$broadcast('weatherUpdate');
+				},
+				function onError(response) {
+					console.log(response)
+				});
+
+		}
+	}
+})
+
+.factory('getWeatherService', function($http) {
+	return {
+		forSixDays: function(city) {
+			var cityName = city.cityName
+			var country = (typeof city.country !== 'undefined' && city.country) ? ',' + city.country : '';
+			var url = `http://api.openweathermap.org/data/2.5/forecast/daily?q=${cityName}${country}&cnt=6&units=metric&appid=${api_key}`;
+
+			return $http({
+				method: 'GET',
+				url: url
+			});
+		}
+	}
+})
 
 // .factory('getDay', function(){
 // 	return {
